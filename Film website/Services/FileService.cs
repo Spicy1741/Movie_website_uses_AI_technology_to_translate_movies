61cf9ab4998e209c38fd14e465623bcbb6366648
Spicy1741 <166927865+Spicy1741@users.noreply.github.com>
2025-06-16 12:42:23 +0700

    a
    a

---
+++ Film website/Services/FileService.cs
@@ -0,0 +1,6 @@
+﻿namespace Film_website.Services
+{
+    public class FileService
+    {
+    }
+}
--- Film website/obj/Debug/net8.0/Film website.AssemblyInfo.cs
+++ Film website/obj/Debug/net8.0/Film website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Film website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+921e5ab36987d9a494dc861cfca5cf8235007455")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cfcde79c554f018c830af21f79c3632f2b30b45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Film website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Film website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
