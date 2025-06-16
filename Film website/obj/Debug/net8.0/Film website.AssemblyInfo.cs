87907fa993afaf8d062cf7f8fbaf4e25da881a5d
Spicy1741 <166927865+Spicy1741@users.noreply.github.com>
2025-06-16 13:43:25 +0700

    sua loi ErrorViewModel

--- Film website/obj/Debug/net8.0/Film website.AssemblyInfo.cs
+++ Film website/obj/Debug/net8.0/Film website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Film website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cfcde79c554f018c830af21f79c3632f2b30b45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c523ea77c0f3bb50738b1a1e1ad557f091ee63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Film website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Film website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
